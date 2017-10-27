51ec5261b2d555b9c9dd5e2a77ebcc1523ace547
John <swanjohn79@gmail.com>
2017-10-27 11:16:54 +0200

    trill 9

---
+++ PythonApplication1/PythonApplication1/module1.py
@@ -0,0 +1,8 @@
+
+import re
+dict = {"name":"Mark", "age":12, "teacher":True}
+d=str(dict)
+print(d)
+for x in dict.items():
+
+print(d2)
--- PythonApplication1/PythonApplication1/myjson.py
+++ PythonApplication1/PythonApplication1/myjson.py
@@ -2,20 +2,34 @@
 
 
 def json_encode( data ):
-    if isinstance( data, bool ):
-        if data:
-            return "true"
-        else:
-            return "false"
-    elif isinstance( data, ( int, float ) ):
-        return str( data )
-    elif isinstance( data, str ):
-        return "NOT IMPLEMENTED, YOU NEED TO IMPLEMENT THIS YOURSELF"
-    elif isinstance( data, list ):
-        return "NOT IMPLEMENTED, YOU NEED TO IMPLEMENT THIS YOURSELF"
-    elif isinstance( data, dict ):
-        return "NOT IMPLEMENTED, YOU NEED TO IMPLEMENT THIS YOURSELF"
-    else:
-        # All other types do not  need to be implemented - it is OK that they raise an error
-        raise TypeError( "%s is not JSON serializable" % repr( data ) )
+	if isinstance( data, bool ):
+		if data:
+			return "true"
+		else:
+			return "false"
+	elif isinstance( data, ( int, float ) ):
+		return str( data )
+	elif isinstance( data, str ):
+		data2 = '"'
+		for x in data:
+			if x == '\n':
+				data2 += '\\n'
+			else:
+				data2 += x
+		data2 += '"'
+		return data2
+	
+	elif isinstance( data, list ):
+		data2=('[')
+		for x in data:
+			data2 += json_encode(x) + ','
+		data3 = data2[:-1] + ']'
+		return data3
+	elif isinstance( data, dict ):
+		data2= str(data)
+		data3=  data2.replace("'", '"').replace(" ", "").replace("True", "true").replace("False", "false")
+		return data3
+	else:
+		# All other types do not  need to be implemented - it is OK that they raise an error
+		raise TypeError( "%s is not JSON serializable" % repr( data ) )
 
